38837f29b02bb72f2a839531b6852e5d8adeffd3
Fernandez Fraguas <angel.fernandez@evolutio.com>
2020-10-09 13:31:28 +0200

    commit

--- gcloud/js/gcloud.js
+++ gcloud/js/gcloud.js
@@ -1,5 +1,5 @@
 console.log("#################################");
-console.log("#### Versionado v1.1.6         ##");
+console.log("#### Versionado v1.1.8         ##");
 console.log("#### Evolutio                  ##");
 console.log("#### Genesys Cloud Scripts     ##");
 console.log("#################################");
